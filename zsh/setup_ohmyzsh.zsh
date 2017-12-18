0a8b210e87bcc32eacac94d5a05b8ee5b515e569
Jordan R Willis <jwillis0720@gmail.com>
2017-12-18 11:31:26 -0800

    changing themesj on hal10k

--- zsh/setup_ohmyzsh.zsh
+++ zsh/setup_ohmyzsh.zsh
@@ -1,7 +1,7 @@
 if [ -d "/dnas" ]; then
   ZSH_THEME="xiong-chiamiov-plus"
 else
-  ZSH_THEME="jispwoso"
+  ZSH_THEME="xiong-chiamiov-plus"
 export ZSH=$HOME/.oh-my-zsh
 fi
 ENABLE_CORRECTION="true"
